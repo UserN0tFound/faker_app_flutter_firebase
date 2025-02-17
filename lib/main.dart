ac69de62c8e347aff3030a1be13c96bb18fa4f99
UserN0tFound <ramtin.chitsaz@student.vinci.be>
2025-02-17 14:24:39 +0100

    configuration de firebase dans projet de départ

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,21 @@ import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
