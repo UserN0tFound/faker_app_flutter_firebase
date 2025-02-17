0ef82f70f506943a53c4dab043d71fe93fd47d0b
UserN0tFound <ramtin.chitsaz@student.vinci.be>
2025-02-17 16:05:54 +0100

    fix android compilation

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-
-
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
-
+  );
 
   runApp(const ProviderScope(child: MyApp()));
 }
--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,9 +7,15 @@ class CustomSignInScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // ajout de widget pour la connexion via firebase_ui_auth
+    // pleins de choses déjà préconfigurées
+    final authProviders = [EmailAuthProvider()];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign in'),
+      ),
+      body: SignInScreen(
+        providers: authProviders,
       ),
     );
   }
