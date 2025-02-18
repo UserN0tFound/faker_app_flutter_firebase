6239cc683d4e7d1fe2eb9004c4cbb4e39937a84b
UserN0tFound <ramtin.chitsaz@student.vinci.be>
2025-02-18 14:46:41 +0100

    ajout de méthode update/delete

--- lib/src/data/firestore_repository.dart
+++ lib/src/data/firestore_repository.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker_app_flutter_firebase/src/data/job.dart';
-import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FirestoreRepository {
@@ -8,17 +8,29 @@ class FirestoreRepository {
 
   final FirebaseFirestore _firestore;
 
-  Future<void> addJob(String uid, String title, String company) async {
-    final docRef = await _firestore.collection('jobs').add({
-      // on fait ça pour l'instant mais c'est une erreur
-      'uid': uid,
-      'title': title,
-      'company': company
-    });
+  // ajout d'un job dans la collection 'jobs'
+  Future<void> addJob(String uid, String title, String company) =>
+      _firestore.collection('jobs').add({
+        // on fait ça pour l'instant mais c'est une erreur
+        'uid': uid,
+        'title': title,
+        'company': company
+      });
 
-    debugPrint(docRef.id);
-  }
+  // update d'un job dans la collection 'jobs'
+  Future<void> updateJob(
+          String uid, String jobId, String title, String company) =>
+      _firestore.doc('jobs/$jobId').update({
+        // on fait ça pour l'instant mais c'est une erreur
+        'uid': uid,
+        'title': title,
+        'company': company
+      });
 
+  Future<void> deleteJob(String uid, String jobId) =>
+      _firestore.doc('jobs/$jobId').delete();
+
+  // récuperation de la collection 'jobs' et conversion en Job
   Query<Job> jobsQuery() {
     return _firestore.collection('jobs').withConverter(
         fromFirestore: (snapshot, options) => Job.fromMap(snapshot.data()!),
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:faker/faker.dart' hide Job;
 import 'package:faker_app_flutter_firebase/src/data/firestore_repository.dart';
 import 'package:faker_app_flutter_firebase/src/data/job.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -50,9 +51,40 @@ class JobsListView extends ConsumerWidget {
       query: firestoreRepository.jobsQuery(),
       itemBuilder: (context, doc) {
         final job = doc.data();
-        return ListTile(
-          title: Text(job.title),
-          subtitle: Text(job.company),
+        return Dismissible(
+          // id du document
+          key: ValueKey(doc.id),
+          // couleur de fond lors du swipe
+          background: ColoredBox(color: Colors.red),
+          // direction du swipe
+          direction: DismissDirection.endToStart,
+
+          // action lors du swipe
+          onDismissed: (direction) {
+            final user = ref.read(firebaseAuthProvider).currentUser;
+
+            ref.read(firestoreRepositoryProvider).deleteJob(
+                  user!.uid,
+                  doc.id,
+                );
+          },
+          child: ListTile(
+            title: Text(job.title),
+            subtitle: Text(job.company),
+            onTap: () {
+              final user = ref.read(firebaseAuthProvider).currentUser;
+              final faker = Faker();
+              final title = faker.job.title();
+              final company = faker.company.name();
+
+              ref.read(firestoreRepositoryProvider).updateJob(
+                    user!.uid,
+                    doc.id,
+                    title,
+                    company,
+                  );
+            },
+          ),
         );
       },
     );
