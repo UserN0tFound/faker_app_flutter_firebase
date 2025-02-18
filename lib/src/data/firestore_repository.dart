ac35d47c0c4fa379bc214e03ba45ee06263598fe
UserN0tFound <ramtin.chitsaz@student.vinci.be>
2025-02-18 13:56:26 +0100

    ajout repository (firestore)

---
+++ lib/src/data/firestore_repository.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FirestoreRepository {
+  FirestoreRepository(this._firestore);
+
+  final FirebaseFirestore _firestore;
+
+  Future<void> addJob(String uid, String title, String company) async {
+    final docRef = await _firestore.collection('jobs').add({
+      // on fait ça pour l'instant mais c'est une erreur
+      'uid': uid,
+      'title': title,
+      'company': company
+    });
+
+    debugPrint(docRef.id);
+  }
+}
+
+final firestoreRepositoryProvider = Provider<FirestoreRepository>((ref) {
+  return FirestoreRepository(FirebaseFirestore.instance);
+});
