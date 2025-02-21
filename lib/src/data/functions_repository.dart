5e86d146c4cc7eceeb82e6e853741a4825532758
UserN0tFound <ramtin.chitsaz@student.vinci.be>
2025-02-21 01:04:50 +0100

    M5 11 : Invoking an HTTP callable function from the Flutter client

--- lib/src/data/functions_repository.dart
+++ lib/src/data/functions_repository.dart
@@ -1,12 +1,21 @@
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FunctionsRepository {
   FunctionsRepository(this._functions);
   final FirebaseFunctions _functions;
 
+  // Méthode pour appeler la fonction Cloud 'deleteAllUserJobs'
   Future<void> deleteAllUserJobs() async {
-    // TODO: Implement
+    // Créer un objet callable pour la fonction Cloud 'deleteAllUserJobs'
+    final callable = _functions.httpsCallable('deleteAllUserJobs');
+
+    // Appeler la fonction Cloud et attendre le résultat
+    final result = await callable();
+
+    // Afficher le résultat dans la console pour le débogage
+    debugPrint(result.data.toString());
   }
 }
 
